403806f916b1dcd9bb73881f440c6a8f287f10d0
datsukan <s.datsukan@gmail.com>
2022-09-19 06:03:29 +0900

    OGP画像の設定

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -15,7 +15,7 @@ const defaultMeta = {
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: '/svg/Vercel.svg',
+  image: '/images/case-converter-og.png',
 };
 
 type SeoProps = {
